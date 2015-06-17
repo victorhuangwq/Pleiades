866b0b2b9e3682fa8dc7add6da745a270b6f19ce
XtrKiL <xtrkil146@gmail.com>
2015-06-17 10:11:26 +0800

    Adds rudimentary line drawing functionality

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -2,12 +2,19 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function() {
+    // Javascript object to store all map data
+    var map_data = {name:"Untitled", elements:[]};
+    
     var selected = 0;
     $('#selectdiv').hide();
-    $('#drawbutton').hide();
-    $('#landmarkbutton').hide();
-    $('#removebutton').hide();
-    var map_canvas = $("#canvas");
+    $('#drawdiv').hide();
+    $('#landmarkdiv').hide();
+    $('#removediv').hide();
+    
+    //map_canvas properties
+    var map_canvas = $('#canvas');
+    map_canvas.width($('#app').width());
+    map_canvas.height($('#app').height());
     var ctx = canvas.getContext("2d");
     
     $('#selectbutton').click( function() {
@@ -40,9 +47,9 @@ $(document).ready(function() {
         $('#landmarkbutton').attr("disabled", false);
         $('#removebutton').attr("disabled", false);
         $('#selectdiv').hide();
-        $('#drawbutton').hide();
-        $('#landmarkbutton').hide();
-        $('#removebutton').hide();
+        $('#drawdiv').hide();
+        $('#landmarkdiv').hide();
+        $('#removediv').hide();
         
         switch (selected) {
         case 1:
@@ -68,26 +75,36 @@ $(document).ready(function() {
     
     var penDown = false;
     var x1, y1;
-
-    $('map_canvas').on('mousedown',function(e){
-        if (penDown === false) {
+    
+    function getMousePos(e) {
+        return {
+            x: e.offsetX,
+            y: e.offsetY
+        };
+    }
+    
+    map_canvas.on('mousedown',function(e) {
+        if (penDown === false && selected == 2) {
             penDown = true;
-            var pos = getMousePos(canvas, e);
+            var pos = getMousePos(e);
             x1 = pos.x;
             y1 = pos.y;
         }
     });
     
-    $('map_canvas').on('mouseup',function(e){
-        if (penDown === true) {
+    map_canvas.on('mouseup',function(e) {
+        if (penDown === true && selected == 2) {
             penDown = false;
-            var pos = getMousePos(canvas, e);
+            var pos = getMousePos(e);
             ctx.beginPath();
             ctx.strokeStyle = "blue";
             ctx.moveTo(x1,y1);
+            console.log("Start:"+x1+","+y1);
             ctx.lineTo(pos.x,pos.y);
+            console.log("End:"+pos.x+","+pos.y);
             ctx.stroke();
             ctx.closePath();
+            map_data['elements'].push({type: "line", start: {x: x1, y: y1}, end: {x: pos.x, y: pos.y}});
         }
     });
     
