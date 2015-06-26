8b0951f7d6977fa6d5bc85576e155e9d04974ecc
Chua Hou <chua.hou@gmail.com>
2015-06-26 10:03:19 +0800

    Updates log

--- heroku.sh
+++ heroku.sh
@@ -1,8 +1,13 @@
 # script to quickly push to heroku
+echo "Cleaning and precompiling rake assets..."
 rake assets:clean
 rake assets:precompile
+echo "Commiting to git..."
 git add -A
 git commit -m "Processing for heroku..."
+echo "Pushing to heroku..."
 git push heroku
+echo "Restarting heroku dynos..."
 heroku restart
+echo "Cleaning assets..."
 rake assets:clean
