aaafd912d7464c19989cf523d49ca9fe90411680
XtrKiL <xtrkil146@gmail.com>
2015-07-14 14:44:06 +0800

    Makes submit page a redirect page

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,20 +1,7 @@
 $(document).ready(function() {
-
   //Copy and pasting code
-  var copy_link_button = $('#copyLinkButton');
-  copy_link_button.disabled = !document.queryCommandSupported('copy');
-
-  copy_link_button.click(function(){
-    var copy_text_area = $('#copyLinkArea');
-    copy_text_area.select();
-
-    try{
-      var successful = document.execCommand('copy');
-      console.log('Copying text was ' +successful);
-    }
-    catch(err){
-      console.log(err);
-    }
-  });
+  setTimeout(function(){
+    window.location = "/view/view?mapid="+id;
+  }, 1000);
 
 });
