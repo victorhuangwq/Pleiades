0850143ff93a3fad5d1917a9ca335b0197cd1e01
Chua Hou <chua.hou@gmail.com>
2015-07-21 14:32:25 +0800

    Fixes search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,9 @@ class SearchController < ApplicationController
         if params[:search]
             @visibility = "visible"
             @maps = Map.search(params[:search]).order("created_at DESC");
+        else
+            @dummy = Map.create title: ""
+            @maps = [@dummy]
         end
     end
 end
