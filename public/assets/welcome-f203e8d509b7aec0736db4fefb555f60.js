46a658358754ffba69dd64e75f52fcd7d61e93aa
XtrKiL <xtrkil146@gmail.com>
2015-08-04 20:30:57 +0800

    Precompile for heroku

---
+++ public/assets/welcome-f203e8d509b7aec0736db4fefb555f60.js
@@ -0,0 +1,51 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  var introguide = introJs();
+
+  introguide.setOptions({
+    steps: [
+        {
+          element:"#howtogo-title",
+          intro: '<center>HowToGo is all about QuikMaps.</center>\
+        <hr/>Use the <b>arrow keys</b> for navigation or hit <b>ESC</b> to exit the tour immediately.'
+        },
+        {
+          //element:"#compass",
+          intro: "<h4>What are QuikMaps</h4>QuikMaps are simplified maps that gets the direction across to your guests and friends"
+        },
+        {
+          element:"#myNavbar",
+          intro: "Either <b>Search</b> for an existing QuikMap to your destination, or <b>Create</b> one of your own."
+        },
+        {
+          element: "#searchbutton",
+          intro: "Click on <strong>Search</strong> to search existing QuikMaps",
+        },
+        {
+          element: "#createbutton",
+          intro: "Now let's try creating a QuikMap"
+        }
+    ],
+    doneLabel: 'Create QuikMap',
+    disableInteraction: 'false'
+  });
+
+  introguide.oncomplete(function(){
+    window.location.href = '/create/create?introTrue=true';
+  });
+
+
+  $('#tour').click(function(){
+      if(window.innerWidth < 768){
+        $('#myNavbar').collapse('show');
+        $('#myNavbar').on('shown.bs.collapse',function(){
+          introguide.start();
+        });
+      }
+      else{
+        introguide.start();
+      }
+  });
+
+});
